fe2c2e402acdee602c37160e3699e1115ede7259
Carina Estevez Orth <carinaestevezorth@gmail.com>
2024-09-09 19:26:06 +0200

    conflict with docker secrets in mariadb set up

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 # setup.sh
 
-service mariadb start
-
+# Variables for database setup
 DB_NAME=${MYSQL_DATABASE:-thedatabase}
 DB_USER=${MYSQL_USER:-theuser}
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-echo "Initializing MariaDB..."
+# Start MariaDB service
+service mariadb start
 
-mariadb -v -u root << EOF
+# Execute SQL commands
+mariadb -u root -p"$DB_ROOT_PASSWORD" -e "
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
-EOF
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+"
 
-sleep 5
-
+# Stop MariaDB service
 service mariadb stop
 
-echo "Restarting MariaDB..."
-
-exec $@ 
+echo "MariaDB setup complete!"
 
