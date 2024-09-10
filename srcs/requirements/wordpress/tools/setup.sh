e8e424e44918199e89c3a780b6845c10dbd42bc9
Carina Estevez Orth <cestevez@c1b4c1.42berlin.de>
2024-09-10 17:55:17 +0200

    wordpress failing: php-fpm wrong path + docker secret not found

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # setup.sh
 
-# Read secrets from Docker secrets
 DB_NAME=${WORDPRESS_DB_NAME:-wordpress}
-DB_USER=${WORDPRESS_DB_USER:-wpuser}
+DB_USER=${WORDPRESS_DB_USER:-wp_user}
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_HOST=${WORDPRESS_DB_HOST:-mariadb:3306}
 DB_PREFIX=${WORDPRESS_DB_PREFIX:-wp_}
 DB_CHARSET=${WORDPRESS_DB_CHARSET:-utf8}
 DB_COLLATE=${WORDPRESS_DB_COLLATE:-}
-SECRET_KEY=$(cat /run/secrets/wp_secret_key)
+#SECRET_KEY=$(cat /run/secrets/wp_secret_key)
+AUTH_KEY=${WP_AUTH_KEY}
+SECURE_AUTH_KEY=${WP_SECURE_AUTH_KEY}
+LOGGED_IN_KEY=${WP_LOGGED_IN_KEY}
+NONCE_KEY=${WP_NONCE_KEY}
+AUTH_SALT=${WP_AUTH_SALT}
+SECURE_AUTH_SALT=${WP_SECURE_AUTH_SALT}
+LOGGED_IN_SALT=${WP_LOGGED_IN_SALT}
+NONCE_SALT=${WP_NONCE_SALT}
+
 
 # Check if wp-config.php exists, if not, create it
 if [ ! -f /var/www/html/wp-config.php ]; then
@@ -26,14 +34,14 @@ define( 'DB_CHARSET', '${DB_CHARSET}' );
 define( 'DB_COLLATE', '${DB_COLLATE}' );
 \$table_prefix = '${DB_PREFIX}';
 
-define( 'AUTH_KEY', '${SECRET_KEY}' );
-define( 'SECURE_AUTH_KEY', '${SECRET_KEY}' );
-define( 'LOGGED_IN_KEY', '${SECRET_KEY}' );
-define( 'NONCE_KEY', '${SECRET_KEY}' );
-define( 'AUTH_SALT', '${SECRET_KEY}' );
-define( 'SECURE_AUTH_SALT', '${SECRET_KEY}' );
-define( 'LOGGED_IN_SALT', '${SECRET_KEY}' );
-define( 'NONCE_SALT', '${SECRET_KEY}' );
+define( 'AUTH_KEY', '${AUTH_KEY}' );
+define( 'SECURE_AUTH_KEY', '${SECURE_AUTH_KEY}' );
+define( 'LOGGED_IN_KEY', '${LOGGED_IN_KEY}' );
+define( 'NONCE_KEY', '${NONCE_KEY}' );
+define( 'AUTH_SALT', '${AUTH_SALT}' );
+define( 'SECURE_AUTH_SALT', '${SECURE_AUTH_SALT}' );
+define( 'LOGGED_IN_SALT', '${LOGGED_IN_SALT}' );
+define( 'NONCE_SALT', '${NONCE_SALT}' );
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/' );
